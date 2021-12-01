3c55444f7cfea111800d809eca859795114c30a4
khoadang1404 <khoadang.1404@gmail.com>
2021-12-01 14:23:32 +0700

    them moi giao dien lan 1

---
+++ app/src/main/java/com/example/view_appgiaohang/moi_ban_be.java
@@ -0,0 +1,14 @@
+package com.example.view_appgiaohang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class moi_ban_be extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_moi_ban_be);
+    }
+}
